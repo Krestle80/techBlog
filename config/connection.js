59db5f81a9cf2ecaec2afe7af3950e894731a8c5
Kronk <kylec0217@gmail.com>
2022-01-17 01:09:38 -0500

    adding a log and changing the host

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
+console.log(process.env.PSQL_HOST,"congig 3")
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.PSQL_HOST || "localhost",
+      host: process.env.PSQL_HOST ,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,

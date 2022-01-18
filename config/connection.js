2012e29de6d90cf70ed3b7ca105a2c79774d2af0
Kronk <kylec0217@gmail.com>
2022-01-18 11:20:02 -0500

    pork in sequalize

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 const Sequelize = require('sequelize');
-const {PORT} = require('../server')
 require('dotenv').config();
-console.log(PORT,"congig 3")
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: PORT,
+      host: '127.0.0.1',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
